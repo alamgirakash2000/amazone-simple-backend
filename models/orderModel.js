3645dd460fe4134f0628d8c3e904dff8cf20308e
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-10-09 23:15:22 +0600

    order with payment info

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
-const instance = mongoose.Schema({
-  email: String,
-  phone: String,
-  address: String,
-  timestamp: Date,
-  items: Array,
-  price: Number,
-  shipment: Number,
-});
+const instance = mongoose.Schema(
+  {
+    email: String,
+    phone: String,
+    address: String,
+    items: Array,
+    price: Number,
+    paymentInfo: Object,
+    shipment: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("orders", instance);
